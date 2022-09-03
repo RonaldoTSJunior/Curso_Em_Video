7d45146fed5801661226eac3064c930cd29a575e
Ronaldo <ronaldojunior2888@gmail.com>
2022-09-02 23:47:07 -0300

    Adicionado sleep de 1s

--- Desafios_e_programas/pedra_papel_tesoura.py
+++ Desafios_e_programas/pedra_papel_tesoura.py
@@ -4,6 +4,7 @@ PAPEL GANHA DE PEDRA
 TESOURA GANHA DE PAPEL
 '''
 import random
+from time import sleep
 jogar_novamente='s'
 while jogar_novamente=='s':
     print('''------BEM VINDO AO PEDRA PAPEL OU TESOURA!------
@@ -14,27 +15,42 @@ while jogar_novamente=='s':
     computador=random.randint(1,3)
     p1=int(input('Digite sua escolha: '))
     if computador==1:
+        sleep(1)
         print('Computador escolheu PEDRA ')
+        sleep(1)
         if p1==1:
             print('EMPATE!')
+            sleep(1)
         elif p1==2:
             print('Player ganhou!')
+            sleep(1)
         elif p1==3:
             print('Computador ganhou! ')
+            sleep(1)
     elif computador==2:
+        sleep(1)
         print('Computador escolheu PAPEL ')
+        sleep(1)
         if p1==1:
             print('Computador ganhou! ')
+            sleep(1)
         elif p1==2:
             print('EMPATE! ')
+            sleep(1)
         elif p1==3:
             print('Player ganhou! ')
+            sleep(1)
     elif computador==3:
+        sleep(1)
         print('Computador escolheu TESOURA ')
+        sleep(1)
         if p1==1:
             print('Player ganhou! ')
+            sleep(1)
         elif p1==2:
             print('Computador ganhou! ')
+            sleep(1)
         elif p1==3:
             print('EMPATE! ')
+            sleep(1)
     jogar_novamente=str(input('Deseja jogar novamente? s/n   ')).lower()
